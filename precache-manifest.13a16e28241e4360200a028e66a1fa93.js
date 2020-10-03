2e0ab5a5f2d66fa69a9919f01fbf6247cdf94d72
Shahrukh Qureshi <shaleequreshi2019@gmail.com>
2020-10-03 01:40:27 -0400

    Updates

---
+++ precache-manifest.13a16e28241e4360200a028e66a1fa93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed54a7f8d3d2ec35df71716cf092222d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af4ad56a4d55b729d60",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5e6681c05d90eb1a67af",
+    "url": "/static/css/main.caf7d372.chunk.css"
+  },
+  {
+    "revision": "3af4ad56a4d55b729d60",
+    "url": "/static/js/2.f1a75d0c.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/static/js/2.f1a75d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6681c05d90eb1a67af",
+    "url": "/static/js/main.df829478.chunk.js"
+  },
+  {
+    "revision": "ca89c15edc745b2408a5",
+    "url": "/static/js/runtime-main.b36854eb.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "4b96047e4af086277cdaeb9e60857534",
+    "url": "/static/media/Ubuntu-Italic.4b96047e.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "/static/media/Ubuntu-Light.277289c5.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  }
+]);

44a6ed8be83f69e29a87329d420affb94613d19a
Shahrukh Qureshi <shaleequreshi2019@gmail.com>
2020-10-02 23:49:32 -0400

    Updates

---
+++ precache-manifest.0e659301a55718dfb3acf3239fcad384.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80927991fd67d5aa0f78f0deac297300",
+    "url": "/NorthHacks/index.html"
+  },
+  {
+    "revision": "ac4ad6484dfb5ee0b863",
+    "url": "/NorthHacks/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d2151c8b1b9e6940dcc6",
+    "url": "/NorthHacks/static/css/main.0411a2f0.chunk.css"
+  },
+  {
+    "revision": "ac4ad6484dfb5ee0b863",
+    "url": "/NorthHacks/static/js/2.40b91a0e.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/NorthHacks/static/js/2.40b91a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2151c8b1b9e6940dcc6",
+    "url": "/NorthHacks/static/js/main.05eb8116.chunk.js"
+  },
+  {
+    "revision": "e096207cb0b0c0d6d43d",
+    "url": "/NorthHacks/static/js/runtime-main.1d4fc524.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/NorthHacks/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "4b96047e4af086277cdaeb9e60857534",
+    "url": "/NorthHacks/static/media/Ubuntu-Italic.4b96047e.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "/NorthHacks/static/media/Ubuntu-Light.277289c5.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/NorthHacks/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  }
+]);

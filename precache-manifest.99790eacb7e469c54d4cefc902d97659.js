6cf9bbc8a6f7dc0d70f3623bc7569e743cbb68e4
Shahrukh Qureshi <shaleequreshi2019@gmail.com>
2020-10-03 11:13:45 -0400

    Updates

--- precache-manifest.99790eacb7e469c54d4cefc902d97659.js
+++ precache-manifest.99790eacb7e469c54d4cefc902d97659.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8f7225f78b01fde6af6b2d866037b85",
+    "revision": "d4d4dca3b307fd8fa8e075e96a8beb71",
     "url": "/NorthHacks/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/NorthHacks/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "e9519dd6016d8372e7c9",
+    "revision": "1e4d4f6b0cafffc30b91",
     "url": "/NorthHacks/static/css/main.0411a2f0.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/NorthHacks/static/js/2.af13bfc7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e9519dd6016d8372e7c9",
-    "url": "/NorthHacks/static/js/main.df829478.chunk.js"
+    "revision": "1e4d4f6b0cafffc30b91",
+    "url": "/NorthHacks/static/js/main.957739c3.chunk.js"
   },
   {
     "revision": "e096207cb0b0c0d6d43d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NorthHacks/precache-manifest.cfe5783d7e7ae4dcd5fdc58a0f26c0f2.js"
+  "/NorthHacks/precache-manifest.99790eacb7e469c54d4cefc902d97659.js"
 );
 
 self.addEventListener('message', (event) => {
